dd692e1c5090200b29eb893d03b95ad5f0e38d38
HajaSC <hajasc@protonmail.com>
2020-04-26 00:37:11 -0400

    Array Deconstructuring

---
+++ manyswap.js
@@ -0,0 +1,10 @@
+const crewmembers = [
+ 'Alex Kamal',
+ 'James Holden',
+ 'Naomi Nagata',
+];
+
+const [pilot, captain, engineer] = crewmembers;
+console.log(engineer);
+console.log(pilot);
+console.log(captain);
