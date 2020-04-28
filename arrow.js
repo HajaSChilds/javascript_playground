087cbeac0c186a8903483a3a6d913987cce293bb
HajaSC <hajasc@protonmail.com>
2020-04-28 13:25:49 -0400

    Function and Arrow Function Limitations with Nested Functions

---
+++ arrow.js
@@ -0,0 +1,13 @@
+function DinnerBill(meal1,meal2) {
+    this.meal1 = meal1;
+    this.meal2 = meal2;
+    this.tax = 0.15;
+ 
+    this.subtotal  = this.tax * (this.meal1 +this.meal2);
+   
+}
+
+const mybill = new DinnerBill(35.00, 40.00);
+console.log(mybill);
+
+
