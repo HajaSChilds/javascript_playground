f7c89e736954143067c612d7eac69b3e5b712d59
HajaSC <hajasc@protonmail.com>
2020-04-30 23:46:00 -0400

    Javascript Classes

--- oop-intro.js
+++ oop-intro.js
@@ -1,8 +1,9 @@
 class Instructor {
-    constructor({name}){
+    constructor({name, password}){
         this.name = name;
+        this.password = password;
     }
 }
 
-const jon = new Instructor({name: 'Jon Snow'});
+const jon = new Instructor({name: 'Jon Snow', password: 12345});
 console.log(jon)
