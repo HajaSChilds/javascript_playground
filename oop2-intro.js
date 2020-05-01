a68dbc994230c0f967447d2e64c67b246c02a4f3
HajaSC <hajasc@protonmail.com>
2020-05-01 12:50:22 -0400

    Object Oriented Programming Instantiation and Default Values

---
+++ oop2-intro.js
@@ -0,0 +1,19 @@
+class Instructor {
+    constructor({name, role = 'assistant'}) {
+        this.name = name;
+        this.role = role;
+    }
+
+    renderDetails() {
+        console.log(`${this.name}: ${this.role}`);
+    }
+}
+
+const jon = new Instructor({name: 'Jon Snow'});
+const brayden = new Instructor({name: 'Brayden', role: 'Teacher'})
+console.log(jon.name);
+console.log(jon);
+console.log(brayden);
+jon.renderDetails();
+brayden.renderDetails();
+
