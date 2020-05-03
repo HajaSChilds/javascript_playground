a6b0e75ff23a53d32d96e1f0565cbf21b87a68ad
HajaSC <hajasc@protonmail.com>
2020-05-03 19:56:53 -0400

    Promises Using Fetch. Doesnt work in vs Code

---
+++ more_promises.js
@@ -0,0 +1,19 @@
+
+console.log('Starting Fetch call');
+const postsPromise = fetch('https://api.dailysmarty.com/posts')
+console.log('After Fetch call');
+console.log(postsPromise);
+console.log('After program has run');
+    
+
+postsPromise
+.then(data => data.json()) 
+.then(data => {
+    data.posts.forEach((item) => {
+       console.log(item.url_for_post);      
+    });
+})
+
+.catch((error) => {
+    console.log(error);
+});
