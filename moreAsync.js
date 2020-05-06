5a722f44b3ed84d56faa8a49a0d5d8292691e13c
HajaSC <hajasc@protonmail.com>
2020-05-06 18:15:33 -0400

    More javascript practice, Promises, Lodash Javascript Library

---
+++ moreAsync.js
@@ -0,0 +1,25 @@
+const login = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("User logged in...");
+    }, 2000);
+  });
+};
+
+const updateAccount = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Updating last login...");
+    }, 3000);
+  });
+};
+
+async function loginActivities() {
+  const returnedUpdateAccount = await updateAccount();
+  console.log(returnedUpdateAccount);
+
+  const returnedLogin = await login();
+  console.log(returnedLogin);
+}
+
+loginActivities();
